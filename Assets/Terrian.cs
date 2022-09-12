98e0cc9757b6f732bb4f0f7c96103d3d9193596d
wtlllll <3200102777@zju.edu.cn>
2022-09-12 22:07:21 +0800

    清除重复点v0.1

--- Assets/Terrian.cs
+++ Assets/Terrian.cs
@@ -54,47 +54,84 @@ public class Terrian : MonoBehaviour
     public void GenerateNoise()
     {
         ClearMeshData();
-
+        Vector4Int[,] indexDPList = new Vector4Int[N, N];
+        int[] indexList = new int[4];
         //生成顶点
         for (int z = 0; z < N; z++)
             for (int x = 0; x < N; x++)
             {
-                int index = (N * z + x) * 4;
-                float h = Mathf.Round(Mathf.PerlinNoise(x * noiseParam + offsetX, z * noiseParam + offsetZ) * height);
-                vertices.Add(new Vector3(x-0.5f, h, z+0.5f));
-                vertices.Add(new Vector3(x+0.5f, h, z+0.5f));
-                vertices.Add(new Vector3(x+0.5f, h, z-0.5f));
-                vertices.Add(new Vector3(x-0.5f, h, z-0.5f));
-
-                int indexLeft = x > 0 ? (N * z + x - 1) * 4 : -1;
-                int indexDown = z > 0 ? (N * (z - 1) + x) * 4 : -1;
-                if (indexLeft != -1 && Vector3.Distance(vertices[indexLeft + 1], vertices[index]) > 0.1f)
+                int h = (int)Mathf.Round(Mathf.PerlinNoise(x * noiseParam + offsetX, z * noiseParam + offsetZ) * height);
+                if (x==0&&z==0)
                 {
-                    triangles.Add(indexLeft + 1);
-                    triangles.Add(index);
-                    triangles.Add(indexLeft + 2);
-                    triangles.Add(index);
-                    triangles.Add(index + 3);
-                    triangles.Add(indexLeft + 2);
+                    indexDPList[x, z].x = AddVertex(new Vector3(x - 0.5f, h, z + 0.5f));
+                    indexDPList[x, z].y = AddVertex(new Vector3(x + 0.5f, h, z + 0.5f));
+                    indexDPList[x, z].z = AddVertex(new Vector3(x + 0.5f, h, z - 0.5f));
+                    indexDPList[x, z].w = AddVertex(new Vector3(x - 0.5f, h, z - 0.5f));
                 }
-                if (indexDown != -1 && Vector3.Distance(vertices[indexDown], vertices[index + 3]) > 0.1f)
+                else if (z == 0)
                 {
-                    triangles.Add(index + 2);
-                    triangles.Add(indexDown + 1);
-                    triangles.Add(indexDown);
-                    triangles.Add(indexDown);
-                    triangles.Add(index+3);
-                    triangles.Add(index + 2);
+                    if (Vector3.Distance(vertices[indexDPList[x - 1, z].y], new Vector3(x - 0.5f, h, z + 0.5f)) < 0.1f)
+                    {
+                        indexDPList[x, z].x = indexDPList[x - 1, z].y;
+                        indexDPList[x, z].w = indexDPList[x - 1, z].z;
+                        indexDPList[x, z].y = AddVertex(new Vector3(x + 0.5f, h, z + 0.5f));
+                        indexDPList[x, z].z = AddVertex(new Vector3(x + 0.5f, h, z - 0.5f));
+                    }
+                    else
+                    {
+                        indexDPList[x, z].x = AddVertex(new Vector3(x - 0.5f, h, z + 0.5f));
+                        indexDPList[x, z].y = AddVertex(new Vector3(x + 0.5f, h, z + 0.5f));
+                        indexDPList[x, z].z = AddVertex(new Vector3(x + 0.5f, h, z - 0.5f));
+                        indexDPList[x, z].w = AddVertex(new Vector3(x - 0.5f, h, z - 0.5f));
+                    }
                 }
+                else if (x == 0)
+                {
+                    if (Vector3.Distance(vertices[indexDPList[x, z-1].x], new Vector3(x - 0.5f, h, z - 0.5f)) < 0.1f)
+                    {
+                        indexDPList[x, z].z = indexDPList[x, z-1].y;
+                        indexDPList[x, z].w = indexDPList[x, z-1].x;
+                        indexDPList[x, z].x = AddVertex(new Vector3(x - 0.5f, h, z + 0.5f));
+                        indexDPList[x, z].y = AddVertex(new Vector3(x + 0.5f, h, z + 0.5f));
+                    }
+                    else
+                    {
+                        indexDPList[x, z].x = AddVertex(new Vector3(x - 0.5f, h, z + 0.5f));
+                        indexDPList[x, z].y = AddVertex(new Vector3(x + 0.5f, h, z + 0.5f));
+                        indexDPList[x, z].z = AddVertex(new Vector3(x + 0.5f, h, z - 0.5f));
+                        indexDPList[x, z].w = AddVertex(new Vector3(x - 0.5f, h, z - 0.5f));
+                    }
+                }
+                AddTriangle(indexDPList[x, z].x, indexDPList[x, z].y, indexDPList[x, z].z);
+                AddTriangle(indexDPList[x, z].z, indexDPList[x, z].w, indexDPList[x, z].x);
 
-                triangles.Add(index);
-                triangles.Add(index + 1);
-                triangles.Add(index + 2);
-                triangles.Add(index + 2);
-                triangles.Add(index + 3);
-                triangles.Add(index + 0);
+
+                //indexDPList[x, z] = h;
+                //if(h==indexDPList[x-1,z])
+
+
+                //int indexLeft = x > 0 ? (N * z + x - 1) * 4 : -1;
+                //int indexDown = z > 0 ? (N * (z - 1) + x) * 4 : -1;
+                //if (indexLeft != -1 && Vector3.Distance(vertices[indexLeft + 1], vertices[index]) > 0.1f)
+                //{
+                //    triangles.Add(indexLeft + 1);
+                //    triangles.Add(index);
+                //    triangles.Add(indexLeft + 2);
+                //    triangles.Add(index);
+                //    triangles.Add(index + 3);
+                //    triangles.Add(indexLeft + 2);
+                //}
+                //if (indexDown != -1 && Vector3.Distance(vertices[indexDown], vertices[index + 3]) > 0.1f)
+                //{
+                //    triangles.Add(index + 2);
+                //    triangles.Add(indexDown + 1);
+                //    triangles.Add(indexDown);
+                //    triangles.Add(indexDown);
+                //    triangles.Add(index+3);
+                //    triangles.Add(index + 2);
+                //}
             }
-        
+
         //构造网格
         Mesh mesh = new Mesh();
         //构造顶点及其三角面
@@ -113,4 +150,24 @@ public class Terrian : MonoBehaviour
         vertices.Clear();
         triangles.Clear();
     }
+
+    public void AddTriangle(int x,int y,int z)
+    {
+        triangles.Add(x);
+        triangles.Add(y);
+        triangles.Add(z);
+    }    
+
+    public int AddVertex(Vector3 pos)
+    {
+        vertices.Add(pos);
+        return vertices.Count-1;
+    }
+}
+struct Vector4Int
+{
+    public int x;
+    public int y;
+    public int z;
+    public int w;
 }
