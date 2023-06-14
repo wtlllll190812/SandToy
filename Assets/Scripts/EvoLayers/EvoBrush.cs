fa42aef310fd3116db9ae64960723f25e433b73b
wtlllll <3200102777@zju.edu.cn>
2023-06-14 20:49:42 +0800

    项目升级

--- Assets/Scripts/EvoLayers/EvoBrush.cs
+++ Assets/Scripts/EvoLayers/EvoBrush.cs
@@ -1,4 +1,28 @@
-public class EvoBrush: EvoluteLayer
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class EvoBrush : EvoluteLayer
 {
-    
+    [SerializeField] private Camera cam;
+    [SerializeField] private InputActionAsset input;
+    private InputAction paint;
+    private Collider2D col;
+    private Species specie = Species.Sand;
+
+    private void Start()
+    {
+        col = GetComponent<Collider2D>();
+        paint = input.FindAction("Paint");
+    }
+
+    public void OnPaint(InputAction.CallbackContext context)
+    {
+        var pos = paint.ReadValue<Vector2>();
+        RaycastHit2D hit = Physics2D.Raycast(cam.ScreenToWorldPoint(pos), Vector2.zero);
+        if (hit.collider == col)
+        {
+            Debug.Log("Target" + hit.point);
+        }
+    }
 }
---
+++ Assets/Scripts/Species.cs
@@ -0,0 +1,5 @@
+public enum Species
+{
+    Empty,
+    Sand,
+}
