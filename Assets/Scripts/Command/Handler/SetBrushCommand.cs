9e71357df931ffa5e663859c582d4c47d17eb527
wtlllll <3200102777@zju.edu.cn>
2023-06-27 00:12:17 +0800

    添加一些常见的Command

---
+++ Assets/Scripts/Command/Handler/SetBrushCommand.cs
@@ -0,0 +1,15 @@
+namespace Command
+{
+    public class SetBrushCommand: CommandHandler
+    {
+        public override void HandleCommand(Command command)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool Match(Command command)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
