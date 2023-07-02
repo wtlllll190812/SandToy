d17795f0d34bc503991789e7f027438533fe1ff1
wtlllll <3200102777@zju.edu.cn>
2023-07-02 14:51:56 +0800

    更多UI

---
+++ Assets/Editor/SpriteProcessor.cs
@@ -0,0 +1,20 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    public class SpriteProcessor : AssetPostprocessor
+    {
+        private void OnPreprocessTexture()
+        {
+            var importer = this.assetImporter as TextureImporter;
+            if(importer == null)
+                return;
+            
+            importer.textureType = TextureImporterType.Sprite;
+            importer.spritePixelsPerUnit = 100;
+            importer.filterMode = FilterMode.Point;
+            importer.spriteImportMode = SpriteImportMode.Single;
+        }
+    }
+}
